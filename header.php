848b8a5cb628554086662206b74f5d688ba1fdbd
AdamuTokal <70481720+AdamuTokal@users.noreply.github.com>
2021-07-04 01:39:50 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,19 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>CRUD</title>
-  <link rel="stylesheet" href="css/style.css">
+
+  <title>CRUD OPERATION</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div id="wrapper">
         <div id="header">
             <h1 style= "color:white; font-size:25px; font-family: monospace; ">GAMMA MINI PROJECT GROUP</h1>
-        </div>
-		
-		
-		
-		
-		
+        </div>	
         <div id="menu">
             <ul>
                 <li>
