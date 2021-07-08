9ab40ff741bd065a8a4a80b48dc0b04aae3137bf
AdamuTokal <70481720+AdamuTokal@users.noreply.github.com>
2021-07-08 10:11:00 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include 'header.php';
+?>
+<div id="main-content">
+    <h2>All Records</h2>
+    <?php
+      include 'config.php';
+
+      $sql = "SELECT * FROM student JOIN studentclass WHERE student.sclass = studentclass.cid";
+      $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+
+      if(mysqli_num_rows($result) > 0)  {
+    ?>
+    <table cellpadding="10px">
+        <thead>
+        <th>no</th>
+        <th>Name</th>
+        <th>REGISTRATION NO</th>
+        <th>Department</th>
+        <th>Phone</th>
+        <th>Action</th>
+        </thead>
+        <tbody>
+          <?php
+            while($row = mysqli_fetch_assoc($result)){
+          ?>
+            <tr>
+                <td><?php echo $row['sid']; ?></td>
+                <td><?php echo $row['sname']; ?></td>
+                <td><?php echo $row['saddress']; ?></td>
+                <td><?php echo $row['cname']; ?></td>
+                <td><?php echo $row['sphone']; ?></td>
+                <td>
+                    <a href='edit.php?id=<?php echo $row['sid']; ?>'>Edit</a>
+                    <a href='delete-inline.php?id=<?php echo $row['sid']; ?>'>Delete</a>
+                </td>
+            </tr>
+          <?php } ?>
+        </tbody>
+    </table>
