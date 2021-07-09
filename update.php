81fd8992f426997147a3c41bc1c58f2b7f503eb7
Abdulsalam Fareed Oyindamola <58387966+phareed3310@users.noreply.github.com>
2021-07-09 11:15:06 +0100

    Add files via upload

---
+++ update.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+<div id="main-content">
+    <h2>Edit Record</h2>
+    <form class="post-form" action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+        <div class="form-group">
+            <label>Serial number</label>
+            <input type="text" name="sid" />
+        </div>
+        <input class="submit" type="submit" name="showbtn" value="Show" />
+    </form>
+
+    <?php
+   
