539a34a64b27e9600a437e737c380b5d02283063
Abubakar Abdullahi Hassan <87084997+Abdool21@users.noreply.github.com>
2021-07-14 17:32:14 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -16,4 +16,17 @@ if(mysql_num_rows($result)> 0){
 While($row =mysqli_fetch_assoc(result)){
  ?>
  <form class="post-form" action="updatedata.php" method="post">
+
 <div class="form-group">
+<label>Name</label>
+<input type="hidden" name="sid" value="<?php echo $row['sid']; ?>"/>
+<input type="hidden" name="sname" value="<?php echo $row['sname']; ?>"/>
+</div>
+
+<div class="form-group">
+   <label>Address</label>
+<input type="text" name="saddress" value="<?php echo $row['saddress']; ?>"/>
+</div>
+
+<div class="form-group">
+<label>Class</label>
