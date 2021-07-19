8c6cc8e502d393e046d715ace32c80c10b129d97
v3kthor <60967602+V3kthor@users.noreply.github.com>
2021-07-19 07:33:47 +0100

    Update crud.sql

--- crud.sql
+++ crud.sql
@@ -29,7 +29,49 @@ CREATE TABLE `student` (
 
 INSERT INTO `student` (`sid`, `sname`, `saddress`, `sclass`, `sphone`) VALUES
 
+--
+-- Dumping data for table `studentclass`
+--
 
+INSERT INTO `studentclass` (`cid`, `cname`) VALUES
+(1, 'Bsc'),
+(2, 'Prof'),
+(3, 'Phd'),
+(4, 'Msc');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`sid`);
+
+  ALTER TABLE student MODIFY sphone VARCHAR(11); 
+--
+-- Indexes for table `studentclass`
+--
+ALTER TABLE `studentclass`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `sid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `studentclass`
+--
+ALTER TABLE `studentclass`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
 -- --------------------------------------------------------
 
 --
